82dfa9573408bd2fbdcf7c45f3ba4fd040872564
PSSubramanyaBhat <subramanya@robosoftin.com>
2020-12-16 02:15:38 +0530

    Added transition to sir's approach drop down

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import React from 'react';
 import logo from './logo.svg';
 import Arrow from './components/Arrow/Arrow';
 import { Direction } from './components/Arrow/Arrow';
+import Dropdown from './components/Dropdown/Dropdown';
 
 function App() {
   return (
-    <Arrow direction={Direction.Right} color="red"></Arrow>
-  );
+    // <Arrow direction={Direction.Right} color="red"></Arrow>
+    <Dropdown></Dropdown>
+    );
 }
 
 export default App;
--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,24 +1,24 @@
 import React, { useState } from 'react';
 import Arrow, { Direction } from '../Arrow';
 import styles from './Dropdown.module.css';
+import cn from 'classnames';
 
 const Dropdown = () => {
 	const [show, setShow] = useState(false);
+	const dropdownClassNames = cn(styles.DropdownMenu, {[styles.Hidden]: !show}); 
 	return (
 		<div className={styles.Dropdown}>
 			<div className={styles.Header} onClick={() => setShow((prev: boolean) => !prev)}>
 				<div className={styles.SelectedItemText}> Selected Item text </div>
 				<Arrow direction={show ? Direction.Top : Direction.Bottom} color="white"></Arrow>
 			</div>
-			{show
-				? <div className={styles.DropdownMenu}>
-					<div className={styles.DropdownMenuOptions}>Option 1</div>
-					<div className={styles.DropdownMenuOptions}>Option 2</div>
-					<div className={styles.DropdownMenuOptions}>Option 3</div>
-					<div className={styles.DropdownMenuOptions}>Option 4</div>
-					<div className={styles.DropdownMenuOptions}>Option 5</div>
-				</div>
-				: ''}
+			<div className={dropdownClassNames}>
+				<div className={styles.DropDownOptions}>Option 1</div>
+				<div className={styles.DropDownOptions}>Option 2</div>
+				<div className={styles.DropDownOptions}>Option 3</div>
+				<div className={styles.DropDownOptions}>Option 4</div>
+				<div className={styles.DropDownOptions}>Option 5</div>
+			</div>
 		</div>
 	);
 };
